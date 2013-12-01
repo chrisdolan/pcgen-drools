08688faf6e40d597cac8495a0172f013769de059
Chris Dolan <chris@chrisdolan.net>
2013-11-30 23:53:56 -0600

    add circumstances for cases where bonuses only apply in some scenarios

--- net.chrisdolan.pcgen.drools/src/main/java/net/chrisdolan/pcgen/drools/PCReader.java
+++ net.chrisdolan.pcgen.drools/src/main/java/net/chrisdolan/pcgen/drools/PCReader.java
@@ -18,27 +18,7 @@ public class PCReader {
     }
     public PC read(URL url) throws IOException {
         try {
-            XStream xstream = new XStream() {
-                protected MapperWrapper wrapMapper(final MapperWrapper wrapped) {
-                    return new MapperWrapper(wrapped) {
-                        public String aliasForSystemAttribute(String attribute) {
-                            // XStream treats attributes named "class" specially. So, I change the alias for the special
-                            // attribute so I can use attributes named "class" in, e.g., the <level> element.
-                            return "class".equals(attribute) ? "clazz" : wrapped.aliasForSystemAttribute(attribute);
-                        }
-                        public boolean shouldSerializeMember(@SuppressWarnings("rawtypes") Class definedIn, String fieldName) {
-                            // from http://stackoverflow.com/questions/4409717/java-xstream-ignore-tag-that-doesnt-exist-in-xml
-                            // this drops unknown XML elements
-                            if (ignoreAllUnknowns && definedIn == Object.class) {
-                                return false;
-                            }
-                            return super.shouldSerializeMember(definedIn, fieldName);
-                        }
-                    };
-                }
-            };
-            xstream.autodetectAnnotations(true);
-            xstream.alias("pc", PC.class);
+            XStream xstream = makeXstream();
             Object o = xstream.fromXML(url);
             if (!(o instanceof PC))
                 throw new IOException("Unmarshalled XML is not a PC, but is: " + o.getClass());
@@ -47,6 +27,30 @@ public class PCReader {
         } catch (XStreamException e) {
             throw new IOException(e);
         }
+    }
+    private XStream makeXstream() {
+        XStream xstream = new XStream() {
+            protected MapperWrapper wrapMapper(final MapperWrapper wrapped) {
+                return new MapperWrapper(wrapped) {
+                    public String aliasForSystemAttribute(String attribute) {
+                        // XStream treats attributes named "class" specially. So, I change the alias for the special
+                        // attribute so I can use attributes named "class" in, e.g., the <level> element.
+                        return "class".equals(attribute) ? "clazz" : wrapped.aliasForSystemAttribute(attribute);
+                    }
+                    public boolean shouldSerializeMember(@SuppressWarnings("rawtypes") Class definedIn, String fieldName) {
+                        // from http://stackoverflow.com/questions/4409717/java-xstream-ignore-tag-that-doesnt-exist-in-xml
+                        // this drops unknown XML elements
+                        if (ignoreAllUnknowns && definedIn == Object.class) {
+                            return false;
+                        }
+                        return super.shouldSerializeMember(definedIn, fieldName);
+                    }
+                };
+            }
+        };
+        xstream.autodetectAnnotations(true);
+        xstream.alias("pc", PC.class);
+        return xstream;
     }
 
     public boolean ignoreAllUnknowns() {
